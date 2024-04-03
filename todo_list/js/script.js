4188de6bd79310ad47cdc1dcbbb1f52514506846
Rafael <rafaelpatricio2002@gmail.com>
2024-04-03 04:57:26 -0300

    adicionando mais funcionalidades

--- todo_list/js/script.js
+++ todo_list/js/script.js
@@ -5,6 +5,24 @@ const input_tarefa = document.querySelector('.input_tarefa')
 const enviar = document.querySelector('.enviar')
 const todoList = document.getElementById('todoList')
 
+
+
+function editarTarefa(e){
+  const editando = document.getElementById('editando')
+  const div = e.target.parentNode;
+  const li = div.parentNode;
+  const p = li.querySelector('p');
+  const textoTarefa = p.textContent;
+  editando.value = textoTarefa;
+  editar.classList.add('ativo')
+  //falta fazer o foco ir para o texto para digitar
+}
+function excluirTarefa(event){
+  const div = event.target.parentNode
+  const li = div.parentNode
+  li.remove()
+}
+
 function criarTarefa(){
   const tarefa = input_tarefa.value
   const p = document.createElement('p')
@@ -18,18 +36,24 @@ function criarTarefa(){
   const spanBin = document.createElement('span')
   spanBin.classList.add('icon-bin')
   
+  
   div.appendChild(spanBin)
   div.appendChild(spanPen)
-
+  
   li.appendChild(p)
   li.appendChild(div)
-
+  
   todoList.appendChild(li)
+  spanPen.addEventListener('click',editarTarefa)
+  spanBin.addEventListener('click',excluirTarefa)
+  input_tarefa.value = ''
+  input_tarefa.focus()
 
 }
-
 fechar.addEventListener('click',() => {
   editar.classList.toggle('ativo')
 })
 enviar.addEventListener('click',criarTarefa)
+
+
 // criar a funcao que armazena as tarefas
