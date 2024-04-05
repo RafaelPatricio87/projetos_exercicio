a50e63d031d8a5fcb7288493a7884f286adba033
Rafael <rafaelpatricio2002@gmail.com>
2024-04-05 03:39:10 -0300

    terminando as funcionalidades

--- todo_list/js/script.js
+++ todo_list/js/script.js
@@ -4,18 +4,33 @@ const editar = document.querySelector('.editar')
 const input_tarefa = document.querySelector('.input_tarefa')
 const enviar = document.querySelector('.enviar')
 const todoList = document.getElementById('todoList')
+let verifica = false
 
+function reEditar() {
+  const editando = document.querySelector('.editando');
+  const taskId = editando.getAttribute('data-task-id'); // Obtém o ID da tarefa a ser editada
+  const li = document.querySelector(`#todoList li[data-task-id="${taskId}"]`); // Seleciona o LI correto usando o ID
+  const p = li.querySelector('p');
+  const tarefa = editando.value;
+  p.textContent = tarefa; 
 
+  editar.classList.remove('ativo');
+}
 
 function editarTarefa(e){
-  const editando = document.getElementById('editando')
-  const div = e.target.parentNode;
-  const li = div.parentNode;
-  const p = li.querySelector('p');
-  const textoTarefa = p.textContent;
-  editando.value = textoTarefa;
-  editar.classList.add('ativo')
-  //falta fazer o foco ir para o texto para digitar
+  if(!verifica){
+    const editando = document.getElementById('editando')
+    const div = e.target.parentNode;
+    const li = div.parentNode;
+    const p = li.querySelector('p');
+    const textoTarefa = p.textContent;
+    editando.value = textoTarefa;
+    
+    editar.classList.add('ativo')
+    editando.focus()
+  
+    editando.setAttribute('data-task-id', li.dataset.taskId);
+  }
 }
 function excluirTarefa(event){
   const div = event.target.parentNode
@@ -28,6 +43,25 @@ function criarTarefa(){
   const p = document.createElement('p')
   const li = document.createElement('li')
   const div = document.createElement('div')
+  const div2 = document.createElement('div')
+  const input = document.createElement('input')
+  
+  input.type = 'checkbox'
+  input.classList.add('check')
+
+  input.addEventListener('change', function(event){
+    if(event.target.checked){
+      verifica = true
+       p.classList.add('feito')
+    } else {
+      verifica = false
+      p.classList.remove('feito')
+    }
+});
+  
+  const taskId = Date.now();
+
+  li.setAttribute('data-task-id', taskId);
 
   p.textContent = tarefa
 
@@ -35,16 +69,20 @@ function criarTarefa(){
   spanPen.classList.add('icon-pen')
   const spanBin = document.createElement('span')
   spanBin.classList.add('icon-bin')
-  
+  div2.classList.add('div2')
   
   div.appendChild(spanBin)
   div.appendChild(spanPen)
+  div2.appendChild(input)
+  div2.appendChild(p)
+
   
-  li.appendChild(p)
+  li.appendChild(div2)
   li.appendChild(div)
   
   todoList.appendChild(li)
-  spanPen.addEventListener('click',editarTarefa)
+  
+  spanPen.addEventListener('click', editarTarefa)
   spanBin.addEventListener('click',excluirTarefa)
   input_tarefa.value = ''
   input_tarefa.focus()
@@ -55,5 +93,7 @@ fechar.addEventListener('click',() => {
 })
 enviar.addEventListener('click',criarTarefa)
 
+btn.addEventListener('click',reEditar)
+ 
 
 // criar a funcao que armazena as tarefas
