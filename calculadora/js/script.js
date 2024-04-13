934471ce1f44ab087547b741bb23468001205825
Rafael <rafaelpatricio2002@gmail.com>
2024-04-13 03:13:08 -0300

    calculadora

--- calculadora/js/script.js
+++ calculadora/js/script.js
@@ -2,43 +2,88 @@ const btnsNum = document.querySelectorAll('.num')
 const btnsOp = document.querySelectorAll('.operador')
 const visor = document.querySelector('.visor')
 const igual = document.querySelector('.igual')
-let operator = false
+const virgula = document.querySelector('.ponto')
+const porcento = document.querySelector('.porcento')
+const ce = document.querySelector('.ce')
+const del = document.querySelector('.del')
+let novoNumero = true
+let numeroAnterior;
+let operador ;
+let ponto = false
 
-// calculadora quase pronto 
-// falta a virgula
-// falta adicionar o btn de reset e delete
+const contaPendente = () => operador !== undefined
 
-function soma(){
-  const expressao = visor.textContent.replace(/X/g, '*')
-   const resultado = eval(expressao)
-  console.log(resultado)
+function calcular(){
+  if(contaPendente()){
+    const numeroAtual = parseFloat(visor.textContent)
+    novoNumero = true
+    const resultado = eval(`${numeroAnterior} ${operador} ${numeroAtual}`)
+    mostrarVisor(resultado)
+  }
+
 }
 function mostrarVisor(paran){ 
-  if(visor.textContent == '0' ){
+  if(novoNumero){
     visor.textContent = paran
+    novoNumero = false
   }else{
     visor.textContent += paran
-    operator = false  
+
   }
+
 }
 
-function selectNum(e){
-  const valor = e.target.textContent;
-  mostrarVisor(valor)
+
+function selectNum(texto){
+  const text = texto.target.textContent;
+ 
+mostrarVisor(text)
+
 }
 
-function selectOP(e){
-  const operador = e.target.textContent;
-  if(!operator){
-      mostrarVisor(operador)
-      operator = true
 
-    
+function selectOP(e) {
+  if(!novoNumero){
+    calcular()
+    novoNumero = true
+    operador = e.target.textContent;
+    numeroAnterior = parseFloat(visor.textContent)
+  
   }
   
- 
 }
 
-igual.addEventListener('click', soma)
+
+
+
+
+
+
+function resetDisplay(){
+    visor.textContent = 0
+}
+
+function deleteDisplay() {
+  let textoVisor = visor.textContent.slice(0, -1)
+  visor.textContent = textoVisor
+}
+function adicionaVirgula(){
+  let ultimoEl = visor.textContent[visor.textContent.length - 1];
+  if (!visor.textContent.includes('.') && ultimoEl !== 'X' && ultimoEl !== '+' && ultimoEl !== '-' && ultimoEl !== '/') {
+    visor.textContent  += '.';
+    ponto = true;
+  }
+}
+function ativarIgual(){
+   calcular()
+   operador = undefined
+}
+
+igual.addEventListener('click', ativarIgual)
 btnsNum.forEach(btn => btn.addEventListener('click',selectNum))
 btnsOp.forEach(btn => btn.addEventListener('click',selectOP))
+
+virgula.addEventListener('click', adicionaVirgula)
+ce.addEventListener('click',resetDisplay)
+del.addEventListener('click',deleteDisplay)
+porcento.addEventListener('click', () => visor.textContent += porcento.textContent)
