a0deb9c0b5fdbf92c7039c7824c87f1bdd100b0f
Rafael <rafaelpatricio2002@gmail.com>
2024-04-17 04:43:50 -0300

    adicionando funcionalidades

--- calculadora/js/script.js
+++ calculadora/js/script.js
@@ -1,8 +1,48 @@
 const btns = document.querySelectorAll('[id*=tecla]');
+const operadores = document.querySelectorAll('[id*=operador]');
+const display = document.getElementById('display')
+const igual = document.getElementById('igual')
 
-btns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    console.log(btn.textContent)
-    // so para testar se pega todos os btns
-  })
-})
+let novoNumero = true
+let numeroAnterior
+let operador;
+
+// calculadora quae pronta falta
+// CE C << , e inverter
+
+const operacaoPendente = () => operador !== undefined
+const calcular = () => {
+  novoNumero = true
+  if(operacaoPendente()){
+     const numeroAtual = parseFloat(display.textContent)
+     const resultado = eval(`${numeroAnterior}${operador}${numeroAtual}`) 
+     atualizarDisplay(resultado)  
+  }
+}
+
+const atualizarDisplay = (texto) => {
+   if(novoNumero){
+    display.textContent = texto
+    novoNumero = false
+   }else{
+    display.textContent += texto
+   }
+}
+
+const inserirOperador = (event) => {
+   calcular()
+   operador = event.target.textContent
+   numeroAnterior = display.textContent
+   novoNumero = true
+   
+}
+const selecionarIgual = () => {
+  calcular()
+  operador = undefined
+}
+
+const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent)
+
+igual.addEventListener('click',selecionarIgual)
+operadores.forEach(operador => operador.addEventListener('click',inserirOperador))
+btns.forEach(btn => btn.addEventListener('click',inserirNumero))
