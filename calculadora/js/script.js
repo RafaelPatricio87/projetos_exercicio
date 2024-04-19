ff539a8767ec1089c6961d0e4852f1e94b7ccd73
Rafael <rafaelpatricio2002@gmail.com>
2024-04-19 00:23:04 -0300

    fim da calculadora

--- calculadora/js/script.js
+++ calculadora/js/script.js
@@ -2,6 +2,7 @@ const btns = document.querySelectorAll('[id*=tecla]');
 const operadores = document.querySelectorAll('[id*=operador]');
 const display = document.getElementById('display')
 const igual = document.getElementById('igual')
+document.getElementById('limparDisplay')
 
 let novoNumero = true
 let numeroAnterior
@@ -12,10 +13,12 @@ let operador;
 
 const operacaoPendente = () => operador !== undefined
 const calcular = () => {
+
   novoNumero = true
   if(operacaoPendente()){
      const numeroAtual = parseFloat(display.textContent)
      const resultado = eval(`${numeroAnterior}${operador}${numeroAtual}`) 
+   
      atualizarDisplay(resultado)  
   }
 }
@@ -43,6 +46,26 @@ const selecionarIgual = () => {
 
 const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent)
 
+
 igual.addEventListener('click',selecionarIgual)
 operadores.forEach(operador => operador.addEventListener('click',inserirOperador))
 btns.forEach(btn => btn.addEventListener('click',inserirNumero))
+
+const limparCalculo = () => {
+   operador = undefined
+   numeroAnterior = ''
+   numeroAtual = ''
+   display.textContent = ''
+}
+document.getElementById('limparCalculo').addEventListener('click',limparCalculo)
+
+const limparDisplay = () =>  display.textContent = ''
+document.getElementById('limparDisplay').addEventListener('click', limparDisplay)
+
+const backspace = () => display.textContent = display.textContent.slice(0, -1)
+document.getElementById('backspace').addEventListener('click',backspace)
+
+const inverter = () => {
+   display.textContent = display.textContent * -1
+}
+document.getElementById('inverter').addEventListener('click', inverter)
